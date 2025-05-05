49e8ac835f82ad37e0fc16715cbcf59c42ced537
Angelica Allen <angelicaallen@MacBookPro.attlocal.net>
2025-05-04 19:01:11 -0500

    Add Login.js to fix Netlify build

--- src/login.js
+++ src/login.js
@@ -22,20 +22,8 @@ function Login({ onLogin }) {
   return (
     <form onSubmit={handleSubmit}>
       <h2>Login</h2>
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        required
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        required
-      />
+      <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} required />
+      <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} required />
       <button type="submit">Login</button>
     </form>
   );
