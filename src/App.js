ed02aba9ba1cb5bfd3a4b8dadc2831a808960ea5
Angelica Allen <angelicaallen@MacBookPro.attlocal.net>
2025-05-06 23:08:27 -0500

    Dashboard pt 2

--- src/App.js
+++ src/App.js
@@ -1,35 +1,19 @@
 
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
-import Login from "./Login";
-import Signup from "./Signup";
-import Dashboard from "./Dashboard";
-import logo from "./assets/logo.png";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Login';
+import Signup from './Signup';
+import Dashboard from './Dashboard';
+import './App.css';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [userEmail, setUserEmail] = useState("");
-
-  const handleLogin = (email) => {
-    setIsLoggedIn(true);
-    setUserEmail(email);
-  };
-
-  const handleLogout = () => {
-    setIsLoggedIn(false);
-    setUserEmail("");
-  };
-
   return (
     <Router>
-      <div className="App">
-        <img src={logo} alt="Logo" style={{ width: "180px", margin: "20px auto", display: "block" }} />
-        <Routes>
-          <Route path="/" element={<Login onLogin={handleLogin} />} />
-          <Route path="/signup" element={<Signup onSignup={handleLogin} />} />
-          <Route path="/dashboard" element={isLoggedIn ? <Dashboard userEmail={userEmail} onLogout={handleLogout} /> : <Navigate to="/" />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
     </Router>
   );
 }
